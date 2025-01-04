e6aa1a0d2873c5d1d845c4e08a4def6ddcd8d38d
emmanuelcodechimp <emmanuel@codechimpit.com>
2025-01-04 11:15:57 +0000

    added stagger children to grid items

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { motion } from "motion/react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const GridVariants = {
+    hidden: {
+      opacity: 0
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.25,
+      }
+    }
+  };
+  const gridItemsVariants = {
+    hidden: {
+      opacity: 0
+    },
+    visible: {
+      opacity: 1
+    }
+  };
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex flex-col gap-10 overflow-x-hidden ">
+        <motion.section
+          variants={GridVariants}
+          initial="hidden"
+          whileInView="visible"
+          className="grid grid-cols-3 gap-10 p-10">
+          <motion.div variants={gridItemsVariants} className="bg-slate-800 aspect-square rounded-lg  justify-center flex items-center gap-10 "></motion.div>
+          <motion.div variants={gridItemsVariants} className="bg-slate-800 aspect-square rounded-lg  justify-center flex items-center gap-10 "></motion.div>
+          <motion.div variants={gridItemsVariants} className="bg-slate-800 aspect-square rounded-lg  justify-center flex items-center gap-10 "></motion.div>
+          <motion.div variants={gridItemsVariants} className="bg-slate-800 aspect-square rounded-lg  justify-center flex items-center gap-10 "></motion.div>
+          <motion.div variants={gridItemsVariants} className="bg-slate-800 aspect-square rounded-lg  justify-center flex items-center gap-10 "></motion.div>
+          <motion.div variants={gridItemsVariants} className="bg-slate-800 aspect-square rounded-lg  justify-center flex items-center gap-10 "></motion.div>
+          
+        </motion.section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
